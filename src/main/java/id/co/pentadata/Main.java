7d73d3497d021a6c9d0e8e1d4daf38e0f01a5403
elyusron <mayang.yusron@gmail.com>
2020-11-30 15:40:17 +0700

    add component scan

--- src/main/java/id/co/pentadata/Main.java
+++ src/main/java/id/co/pentadata/Main.java
@@ -1,13 +1,24 @@
 package id.co.pentadata;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 @SpringBootApplication
+@Controller
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "id.co.*")
 public class Main {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
+
+	@RequestMapping("/")
+    public String index() {
+        return "index.html";
+    }
 
 }
